d559d55b5293e29421cc5cd1f864f39f0e71c77b
Alkator-Nori <nais000k@gmail.com>
2019-11-23 00:09:17 +0300

    Update views.py
    used related_name

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -73,7 +73,7 @@ def show_pokemon(request, pokemon_id):
         'previous_evolution': evolution_from
     }
 
-    pokemon_entities = PokemonEntity.objects.select_related('pokemon').filter(pokemon=pokemon)
+    pokemon_entities = pokemon.pokemon_entities.all()
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in pokemon_entities:
